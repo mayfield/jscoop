6c035c2be966cd3e6a2c8ed671b80106f108e8b7
Justin Mayfield <tooker@gmail.com>
2020-12-10 23:48:51 -0700

    Working on some RateLimiter tests for seen issues

--- test/jobs.test.js
+++ test/jobs.test.js
@@ -1,5 +1,9 @@
 import * as jobs from '../src/jobs.js';
 
+function timeout(ms) {
+    return new Promise(resolve => setTimeout(() => resolve('timeout'), ms));
+}
+
 test('RateLimiter', () => {
     new jobs.RateLimiter('foo', {
         period: 1000,
@@ -7,10 +11,34 @@ test('RateLimiter', () => {
     });
 });
 
-test('RateLimiter nonconcurrent use', async () => {
+test('RateLimiter nonconcurrent no block', async () => {
     const rl = new jobs.RateLimiter('test', {
         period: 3600 * 1000, // enough to cause jest to timeout on fail
         limit: 1
     });
     await rl.wait();
 });
+
+test('RateLimiter nonconcurrent block', async () => {
+    const rl = new jobs.RateLimiter('test', {
+        period: 3600 * 1000, // enough to cause jest to timeout on fail
+        limit: 1
+    });
+    await rl.wait();
+    rl.increment();
+    expect(await Promise.race([timeout(100), rl.wait()])).toBe('timeout');
+});
+
+test('RateLimiter concurrent block', async () => {
+    const rl = new jobs.RateLimiter('test', {
+        period: 3600 * 1000, // enough to cause jest to timeout on fail
+        limit: 1
+    });
+    const w1 = rl.wait().then(() => rl.increment());
+    const w2 = rl.wait().then(() => rl.increment());
+    const w3 = rl.wait().then(() => rl.increment());
+    expect(await Promise.race([timeout(100), w1])).toNotBe('timeout');
+    expect(await Promise.race([timeout(100), w2])).toBe('timeout');
+    expect(await Promise.race([timeout(100), w3])).toBe('timeout');
+});
+
