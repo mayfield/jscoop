ba6c35c514ef974cf0c860ecd3c990242f8009a9
Justin Mayfield <tooker@gmail.com>
2020-12-11 19:51:38 -0700

    Tests for jobs

--- test/jobs.test.js
+++ test/jobs.test.js
@@ -1,8 +1,41 @@
 import * as jobs from '../src/jobs.js';
+import {Future} from '../src/futures.js';
+
+/*
+let _t;
+function test(name, cb) {
+    if (_t) {
+        _t = _t.finally(cb);
+    } else {
+        _t = Promise.resolve(cb());
+    }
+    _t.then(() => console.log("pass")).catch(console.error);
+}
+function expect(a) {
+    return {
+        toBe: b => {if (a !== b) throw new Error('not eq')},
+        toBeUndefined: () => {if (a !== undefined) throw new Error("not undef")}
+    };
+}
+*/
+
+const _timeouts = [];
 
 function timeout(ms) {
-    return new Promise(resolve => setTimeout(() => resolve('timeout'), ms));
+    let to;
+    const p = new Promise(resolve => to = setTimeout(() => resolve('timeout'), ms));
+    p.clear = () => clearTimeout(to);
+    _timeouts.push(to);
+    return p;
 }
+
+function clearTimeouts() {
+    for (const x of _timeouts) {
+        clearTimeout(x);
+    }
+    _timeouts.length = 0;
+}
+
 
 test('RateLimiter', () => {
     new jobs.RateLimiter('foo', {
@@ -40,5 +73,68 @@ test('RateLimiter concurrent block', async () => {
     expect(await Promise.race([timeout(50), w2])).toBeUndefined();
     expect(await Promise.race([timeout(50), w3])).toBe('timeout');
     await w3; // for testing to no complain
+    clearTimeouts();
+});
+
+test('UnorderedWorkQueue sanity', async () => {
+    const wq = new jobs.UnorderedWorkQueue();
+});
+
+test('UnorderedWorkQueue put', async () => {
+    const wq = new jobs.UnorderedWorkQueue();
+    await wq.put(timeout(100));
+    expect(wq.pending()).toBe(1);
+    await wq.put(timeout(100));
+    expect(wq.pending()).toBe(2);
+    await timeout(300);
+    expect(wq.pending()).toBe(0);
+    expect(wq.fulfilled()).toBe(2);
+});
+
+test('UnorderedWorkQueue get', async () => {
+    const wq = new jobs.UnorderedWorkQueue();
+    await wq.put(timeout(100));
+    expect(wq.pending()).toBe(1);
+    expect(await wq.get()).toBe('timeout');
+    expect(wq.pending()).toBe(0);
+    expect(wq.fulfilled()).toBe(0);
+});
+
+test('UnorderedWorkQueue maxPending', async () => {
+    const wq = new jobs.UnorderedWorkQueue({maxPending: 1});
+    const f1 = new Future();
+    await wq.put(f1);
+    expect(wq.pending()).toBe(1);
+    const f2 = new Future();
+    expect(await Promise.race([timeout(50), wq.put(f2)])).toBe('timeout');
+    f1.setResult();
+    await timeout(10);
+    expect(wq.pending()).toBe(1);
+    expect(wq.fulfilled()).toBe(1);
+    f2.setResult();
+    await timeout(10);
+    expect(wq.pending()).toBe(0);
+    expect(wq.fulfilled()).toBe(2);
+});
+
+test('UnorderedWorkQueue maxFulfilled', async () => {
+    const wq = new jobs.UnorderedWorkQueue({maxFulfilled: 1});
+    const donezo = new Future();
+    donezo.setResult();
+    await wq.put(donezo);
+    await timeout(10);
+    expect(wq.pending()).toBe(0);
+    expect(wq.fulfilled()).toBe(1);
+    expect(await Promise.race([timeout(50), wq.put(donezo)])).toBe('timeout');
+    expect(wq.pending()).toBe(0);
+    expect(wq.fulfilled()).toBe(1);
+    await wq.get();
+    await timeout(10);
+    expect(wq.pending()).toBe(0);
+    expect(wq.fulfilled()).toBe(1);
+    await wq.get();
+    await timeout(10);
+    expect(wq.pending()).toBe(0);
+    expect(wq.fulfilled()).toBe(0);
 });
 
